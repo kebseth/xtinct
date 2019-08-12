6dd7e176d730ea09d47ca8b4c9aaa648f7bb7ef9
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-12 17:22:20 +0200

    Routes initialize

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,3 @@
 class Animal < ApplicationRecord
-  belongs_to :user
   belongs_to :owner, class_name: 'User'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace: :owner do
+    resources: :animals, only: [:index, :new, :create]
+  end
+  resources: :animals, only: [:index, :show] do
+    resources: :rents, only: [:create]
+  end
+
+  resource: :profil, only: [:edit, :update]
+  resources: :rents, only: [:index] do
+    member do
+      patch :accept
+      patch :refuse
+    end
+  end
 end
