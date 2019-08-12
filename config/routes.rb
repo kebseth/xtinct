18b1fa26fa5381d8093fe3e7d575ddd50c5ba31d
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-12 17:26:21 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace: :owner do
-    resources: :animals, only: [:index, :new, :create]
-  end
-  resources: :animals, only: [:index, :show] do
-    resources: :rents, only: [:create]
+
+  devise_for :users
+
+  # AS LODGER
+  resources :animals, only: [:index, :show] do
+    resources :rents, only: [:create]
   end
 
-  resource: :profil, only: [:edit, :update]
-  resources: :rents, only: [:index] do
+  # AS BOTH
+  resource :profile, only: [:edit, :update]
+
+                    # AS BOTH
+  resources :rents, only: [:index] do
+    # AS OWNER
     member do
       patch :accept
       patch :refuse
     end
+  end
+
+  # AS OWNER
+  namespace :owner do
+    resources :animals, only: [:index, :new, :create]
   end
 end
