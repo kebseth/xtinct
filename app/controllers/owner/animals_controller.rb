9250fa0512acabb37fb80da7c401b94c2ecc9f24
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-13 17:57:48 +0200

    new + create animals

---
+++ app/controllers/owner/animals_controller.rb
@@ -0,0 +1,21 @@
+class Owner::AnimalsController < ApplicationController
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(restaurant_params)
+    @animal.owner = current_user
+    if @animal.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:animal).permit(:species, :name, :description, :address, :price_per_day, :photo)
+  end
+end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -4,9 +4,8 @@ class Animal < ApplicationRecord
   belongs_to :owner, class_name: 'User'
 
   validates :species, presence: true
-  validates :names, presence: true
+  validates :name, presence: true
   validates :description, presence: true
-  validates :disponibility, presence: true
   validates :photo, presence: true
   validates :address, presence: true
   validates :price_per_day, presence: true
