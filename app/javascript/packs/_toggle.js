a5d79362d6c7b0cd28bfa3e74eecc68e10739ff5
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-14 13:41:06 +0200

    form stylé

---
+++ app/javascript/packs/_toggle.js
@@ -0,0 +1,6 @@
+const form_toggle = document.querySelector('.imgt__btn').addEventListener('click', function() {
+  document.querySelector('.cont').classList.toggle('s--signup');
+});
+
+form_toggle();
+export {form_toggle};
