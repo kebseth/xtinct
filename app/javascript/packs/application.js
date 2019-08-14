ce5c125f014031353cb2f3d5f722c1d06118e17f
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-14 16:47:52 +0200

    fix ?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,7 @@ import "bootstrap";
 
 // import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
-import { initMapbox } from '../plugins/init_mapbox';
-
-initMapbox();
-
+import { initMapbox } from './plugins/init_mapbox';
 import { alertDis } from './alertify';
 import { form_toggle } from './_toggle';
 
