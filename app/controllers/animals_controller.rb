09e2756c6830e50278dae3212f085e0cac086531
Julien Philippot <julien.philippot86@gmail.com>
2019-08-15 09:24:19 +0200

    seed update & small fixes

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,5 +1,4 @@
 class AnimalsController < ApplicationController
-
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_animal, only: [:show]
 
@@ -21,23 +20,10 @@ class AnimalsController < ApplicationController
       lat: @animal.latitude,
       infoWindow: render_to_string(partial: "info_window", locals: { animal: @animal })
     }]
+    @rent = Rent.new
   end
 
   private
-
-  def pet_params
-    params.require(:animal).permit(
-      :species,
-      :name,
-      :description,
-      :disponibility,
-      :photo,
-      :price_per_day,
-      :address,
-      :latitude,
-      :longitude
-    )
-  end
 
   def set_animal
     @animal = Animal.find(params[:id])
