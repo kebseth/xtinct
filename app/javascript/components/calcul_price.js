38a2ee63f58c727f141eab093c3c99a09aae59df
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-16 16:18:53 +0200

    temp

--- app/javascript/components/calcul_price.js
+++ app/javascript/components/calcul_price.js
@@ -1,17 +1,17 @@
 const begin = document.getElementById('rent_begin_date');
 const end = document.getElementById('rent_end_date');
-const price_for_live = document.getElementById('price_live').innerText;
 const btn = document.getElementById('btn_live');
 
 if(begin != null){
   begin.addEventListener('change', (event) => {
+      const price_for_live = document.getElementById('price_live').innerText;
       if(end.value != null){
         const date_end = new Date(end.value);
         const date_begin = new Date(begin.value);
         const live_price = (date_end - date_begin) / 86400000;
         let live_p = (price_for_live * live_price);
         console.log(live_p);
-        btn.innerHTML = `<strong>${live_p}<strong> €, Let's go!`;
+        btn.innerHTML = `${live_p} €, Let's go!`;
       }
   });
 };
@@ -19,12 +19,13 @@ if(begin != null){
 if(end != null){
   end.addEventListener('change', (event) => {
       if(begin.value != null){
+        const price_for_live = document.getElementById('price_live').innerText;
         const date_begin = new Date(begin.value);
         const date_end = new Date(end.value);
         let live_price = (date_end - date_begin) / 86400000;
         let live_p = (price_for_live * live_price)
         console.log(live_p);
-        btn.innerText = `<strong>${live_p}<strong> €, Let's go!`;
+        btn.innerText = `${live_p} €, Let's go!`;
       }
   });
 }
