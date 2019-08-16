d4319120d8388654182df3fc0499710645ebebe4
Julien Philippot <julien.philippot86@gmail.com>
2019-08-16 17:56:22 +0200

    parallax

--- app/javascript/plugins/banner.js
+++ app/javascript/plugins/banner.js
@@ -1,7 +1,15 @@
-const parallax = document.getElementById("parallax");
-if(parallax != null){
+// const parallax = document.getElementById("parallax");
+// if(parallax != null){
+// function parallax() {
+//   parallax.style.top = -(window.pageYOffset / 4)+'px';
+// }};
+
+// window.addEventListener("scroll", parallax, false);
+
 function parallax() {
+  var parallax = document.getElementById("parallax");
   parallax.style.top = -(window.pageYOffset / 4)+'px';
-}};
+}
 
 window.addEventListener("scroll", parallax, false);
+
